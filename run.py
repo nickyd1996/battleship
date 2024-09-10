198414dfbe06083324bd4144bd94c0c34137ca81
Nicole Drakes <bfwqi5qp@students.codeinstitute.net>
2024-09-10 20:15:11 +0000

    Initial project setup with flask

--- run.py
+++ run.py
@@ -10,6 +10,17 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('battleships')
-info = SHEET.worksheet('info')
-data = info.get_all_values()
+BoardSetup = SHEET.worksheet('BoardSetup')
+data = BoardSetup.get_all_values()
 print(data)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
